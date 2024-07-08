f6d84d763b0b65b2a2c57e87f4d9ea884b1f64af
Gaurav Raj <techw803@gmail.com>
2024-07-08 20:46:32 +0530

    Enabled speed insights from vercel

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -9,7 +9,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 const inter = Inter({ subsets: ["latin"] });
 import { Analytics } from "@vercel/analytics/react"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import ToasterContext from "../context/ToastContext";
 
 export default function RootLayout({
@@ -30,6 +30,7 @@ export default function RootLayout({
           <ToasterContext />
           {children}
           <Analytics />
+          <SpeedInsights />
           <Footer />
           <ScrollToTop />
         </ThemeProvider>
