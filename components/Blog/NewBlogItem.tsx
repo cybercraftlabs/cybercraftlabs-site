7be33465fa700aa89830183bc656de0f9cc7f426
Gaurav Raj <techw803@gmail.com>
2024-07-19 01:57:06 +0530

    Fixed framer motion error

--- components/Blog/NewBlogItem.tsx
+++ components/Blog/NewBlogItem.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import Image from "next/image";
 import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 import Link from "next/link";
 import React from 'react'
+import { motion } from "framer-motion";
 
 
 interface NewBlogItemProps {
@@ -14,50 +17,70 @@ interface NewBlogItemProps {
 
 const NewBlogItem = ({ slug, title, mainImage, description }: NewBlogItemProps) => {
     return (
-        <CardContainer className="inter-var w-full">
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
-                <CardItem
-                    translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white"
-                >
-                    {`${title.slice(0, 50)}...`}
-                </CardItem>
-                <CardItem
-                    as="p"
-                    translateZ="60"
-                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                >
-                    {`${description.slice(0, 155)}...`}
-                </CardItem>
-                <CardItem translateZ="100" className="w-full mt-4">
-                    <Image
-                        src={mainImage}
-                        height="1000"
-                        width="1000"
-                        className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                        alt="thumbnail"
-                    />
-                </CardItem>
-                <div className="flex justify-between items-center mt-10">
-                    <CardItem
-                        translateZ={20}
-                        as={Link}
-                        href={slug}
-                        target="__blank"
-                        className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                    >
-                        Read Now →
-                    </CardItem>
-                    <CardItem
-                        translateZ={20}
-                        as="button"
-                        className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-                    >
-                        Cyber Security
-                    </CardItem>
-                </div>
-            </CardBody>
-        </CardContainer>
+        <>
+            <motion.div
+                variants={{
+                    hidden: {
+                        opacity: 0,
+                        y: -20,
+                    },
+                    visible: {
+                        opacity: 1,
+                        y: 0,
+                    },
+                }}
+                initial="hidden"
+                whileInView="visible"
+                transition={{ duration: 1, delay: 0.5 }}
+                viewport={{ once: true }}
+                className="animate_top"
+            >
+                <CardContainer className="inter-var w-full">
+                    <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+                        <CardItem
+                            translateZ="50"
+                            className="text-xl font-bold text-neutral-600 dark:text-white"
+                        >
+                            {`${title.slice(0, 50)}...`}
+                        </CardItem>
+                        <CardItem
+                            as="p"
+                            translateZ="60"
+                            className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                        >
+                            {`${description.slice(0, 155)}...`}
+                        </CardItem>
+                        <CardItem translateZ="100" className="w-full mt-4">
+                            <Image
+                                src={mainImage}
+                                height="1000"
+                                width="1000"
+                                className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                                alt="thumbnail"
+                            />
+                        </CardItem>
+                        <div className="flex justify-between items-center mt-10">
+                            <CardItem
+                                translateZ={20}
+                                as={Link}
+                                href={slug}
+                                target="__blank"
+                                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                            >
+                                Read Now →
+                            </CardItem>
+                            <CardItem
+                                translateZ={20}
+                                as="button"
+                                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                            >
+                                Cyber Security
+                            </CardItem>
+                        </div>
+                    </CardBody>
+                </CardContainer>
+            </motion.div>
+        </>
     )
 }
 
